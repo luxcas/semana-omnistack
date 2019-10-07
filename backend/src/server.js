b356e55e07531312f8c57d59e2fb1def0d32af73
Lucas Aquino <lucas@lucasaquino.com.br>
2019-10-07 12:46:51 -0300

    funciona página react

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,18 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const routes = require('./routes');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const routes = require("./routes");
 
 const app = express();
 
-// mongoose.connect('mongodb://omnistack:omnistack@cluster0-shard-00-00-7lkjq.mongodb.net:27017,cluster0-shard-00-01-7lkjq.mongodb.net:27017,cluster0-shard-00-02-7lkjq.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority');
+mongoose.connect(
+  "mongodb://omnistack:omnistack@cluster0-shard-00-00-7lkjq.mongodb.net:27017,cluster0-shard-00-01-7lkjq.mongodb.net:27017,cluster0-shard-00-02-7lkjq.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
 
-mongoose.connect('mongodb://admin:123456@127.0.0.1:27017/admin');
+// mongoose.connect('mongodb://admin:123456@127.0.0.1:27017/admin', { useNewUrlParser: true, useUnifiedTopology: true });
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.listen(3334);
